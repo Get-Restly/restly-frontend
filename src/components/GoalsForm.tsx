e5d98688e8532acb3ee32f9f41a91eda2bdc493d
Marius Buleandra <mbuleandra@gmail.com>
2023-11-23 13:10:13 -0800

    create spec

--- src/components/GoalsForm.tsx
+++ src/components/GoalsForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, {FC} from "react";
-import {Button, Textarea} from "flowbite-react";
+import {Textarea} from "flowbite-react";
 
 interface GoalsFormProps {
   value: string;
