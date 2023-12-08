d4695a84e083cf656868a37a7a7049bc49df6a1d
Marius Buleandra <mbuleandra@gmail.com>
2023-12-08 12:05:17 -0800

    tutorial copy buttons

--- src/tour/steps.ts
+++ src/tour/steps.ts
@@ -1,20 +1,18 @@
 import { type ShepherdOptionsWithType } from 'react-shepherd';
-
-const goalsText = `We are going to create a tutorial that sets up an automated agent that talks like Yoda and makes references from the Star Wars movies.
-Use the $VOCODE_API_KEY for the API calls which require authentication.
-
-Here is what we need to cover:
-1. Create a prompt which gives the instructions to act and speak like Yoda from Star Wars.
-2. Create an Azure voice to use for our agent. Use the "en-US-JasonNeural" voice.
-3. Create an agent using the previously created Prompt and Voice.
-4. List all the numbers available and choose the first one.
-5. Update the phone number above to use the agent we just created.`
+import { TourStepIntroHTML, TourStepGoalsHTML, VOCODE_OPENAPI_URL, GOALS_TEXT } from './elements';
 
 const steps: Array<ShepherdOptionsWithType> = [
     {
       id: 'intro',
       attachTo: { element: '.tour-first', on: 'bottom' },
       buttons: [
+        {
+          classes: 'shepherd-button-primary',
+          text: 'Copy URL',
+          action() {
+            navigator.clipboard.writeText(VOCODE_OPENAPI_URL);
+          },
+        },
         {
           classes: 'shepherd-button-primary',
           text: 'Next',
@@ -28,7 +26,7 @@ const steps: Array<ShepherdOptionsWithType> = [
         enabled: true,
       },
       title: 'Step 1: Load an API spec',
-      text: () => '<div><div>Copy and Paste in the Vocode OpenAPI Spec URL:<div><div style="background-color: #f5f5f5; padding: 8px; margin-top: 8px; white-space: nowrap; overflow: scroll; border: 1px solid gray; border-radius: 4px; ">https://raw.githubusercontent.com/vocodedev/vocode-python/main/docs/openapi.json</div><div>',
+      text: () => TourStepIntroHTML,
       when: {
         show: () => {
           console.log('show step');
@@ -76,6 +74,13 @@ const steps: Array<ShepherdOptionsWithType> = [
         buttons: [
           {
             classes: 'shepherd-button-primary',
+            text: 'Copy Text',
+            action() {
+              navigator.clipboard.writeText(GOALS_TEXT);
+            },
+          },
+          {
+            classes: 'shepherd-button-primary',
             text: 'Back',
             type: 'back'
           },
@@ -85,14 +90,14 @@ const steps: Array<ShepherdOptionsWithType> = [
             type: 'next'
           }
         ],
-        classes: 'w-96',
+        classes: '',
         highlightClass: 'highlight',
         scrollTo: false,
         cancelIcon: {
           enabled: true,
         },
         title: 'Step 2: Write step-by-step goals',
-        text: () => 'Enter the following text: <div style="background-color: #f5f5f5; padding: 8px; margin-top: 8px; border: 1px solid gray; border-radius: 4px; overflow: scroll;"><pre>' + goalsText + '</pre></div>',
+        text: () => TourStepGoalsHTML,
         when: {
           show: () => {
             console.log('show step');
